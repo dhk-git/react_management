895eaacc8f76a77a11f5f87871c115eb1e099a82
dhkwak <dreamaboutme.k@gmail.com>
2020-04-08 20:59:32 +0900

    Create Customer DB And

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,7 +7,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/customers', (req, res)=>{
+//db정보 가져오기
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+console.log(conf);
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
+app.get('/api/customers', (req, res) => {
+    console.log("customers호출");
+    connection.query(
+        "SELECT * FROM customer", (err, rows, fields) => {
+            //console.log(rows);
+            res.send(rows);
+        }
+    );
+
+});
+
+app.get('/api/customersbbb', (req, res)=>{
     res.send(
         [
             {
